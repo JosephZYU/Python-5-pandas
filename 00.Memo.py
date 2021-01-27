2b15e325466411b0c2cbe7935ac699bf2ee045cb
JosephYu <josephyu.careers@outlook.com>
2021-01-27 17:37:16 +0800

    04

--- 00.Memo.py
+++ 00.Memo.py
@@ -1,2 +1,4 @@
 # 🧭 loc is better for human-readable analysis loc更适用于文本形式分析 -> .loc[row, col]
 # 🧭 If there is ONLY one filter dimension -> use df[filter] directly! 如果只有一个维度的过滤，直接使用df[filter]
+
+# 🎯 根据high salary过滤，获得各个国家高薪岗位分布图
