4c494c964b306700c42e95531b87ab5b1f233508
JosephYu <josephyu.careers@outlook.com>
2021-01-27 10:28:22 +0800

    01

--- 01.Getting Started with Data Analysis - Installation and Loading Data.py
+++
@@ -1,9 +0,0 @@
-# /Users/josephyu/Documents/GitHub/data/survey_results_public.csv
-
-import pandas as pd
-
-data_2019 = '/Users/josephyu/Documents/GitHub/data/survey_results_public.csv'
-
-df = pd.read_csv(data_2019)
-
-print(df)
