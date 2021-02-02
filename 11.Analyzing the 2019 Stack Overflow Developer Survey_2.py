504cffa6da58fcc6e8669b7e4eb2faa880eb0038
JosephYu <josephyu.careers@outlook.com>
2021-02-02 09:40:46 +0800

    11_2

--- 11.Analyzing the 2019 Stack Overflow Developer Survey_2.py
+++ 11.Analyzing the 2019 Stack Overflow Developer Survey_2.py
@@ -6,6 +6,8 @@ url = '/Users/josephyu/Documents/GitHub/data/survey_results_public.csv'
 with open(url) as f:
     csv_read = csv.DictReader(f)
 
+    total = 0
+
     language_counter = Counter()
 
     for line in csv_read:
@@ -15,14 +17,18 @@ with open(url) as f:
         # 🧠 counter.update()
         language_counter.update(languages)
 
+        total += 1
 
-print(language_counter)
+# 🧠 counter.most_common(n)
+# print(language_counter.most_common(5))
 
+for language, lan_count in language_counter.most_common(10):
+    lan_pct = lan_count / total * 100
+    print(f'{language}: {lan_pct:.2f}%')
 
-# total = counts['Yes'] + counts['No']
+    # total = counts['Yes'] + counts['No']
+    # yes_pct = counts['Yes'] / total * 100
+    # no_pct = counts['No'] / total * 100
 
-# yes_pct = counts['Yes'] / total * 100
-# no_pct = counts['No'] / total * 100
-
-# print(f'Yes: {yes_pct:.2f}%')
-# print(f'No: {no_pct:.2f}%')
+    # print(f'Yes: {yes_pct:.2f}%')
+    # print(f'No: {no_pct:.2f}%')
